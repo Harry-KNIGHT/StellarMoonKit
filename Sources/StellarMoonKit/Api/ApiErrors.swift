781b87051e0038a6b2a4c920e8e47d2406560d1f
Harry-KNIGHT <eliott.knight@icloud.com>
2023-01-27 20:35:23 +0100

    Fetch one article api call.

--- Sources/StellarMoonKit/Api/ApiErrors.swift
+++ Sources/StellarMoonKit/Api/ApiErrors.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 public enum ApiError: Error {
-	case urlNotFound, dataError, badHttpResponse, someThingWentWrong, noDataForImage, cantFetchImage
+	case urlNotFound, dataError, badHttpResponse, someThingWentWrong, noDataForImage, cantFetchImage, dateError
 }
---
+++ Sources/StellarMoonKit/Api/SearchDateArticleViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  SearchDateArticleViewModel.swift
+//  Stellar
+//
+//  Created by Elliot Knight on 27/01/2023.
+//
+
+import Foundation
+
+
+class SearchDateArticleViewModel {
+	static func searchOneArticle(date: Date) async throws -> AstronomyArticleModel  {
+
+		let currentCalendar = Calendar.current
+		let components = currentCalendar.dateComponents([.year, .day, .month], from: date)
+
+		guard let year = components.year,
+			  let day = components.day,
+			  let month = components.month
+		else { throw ApiError.dateError }
+
+		let articleURL = URL(string:"https://apod.ellanan.com/api?date=\(year)-\(month)-\(day)")
+
+		guard let articleURL else { throw ApiError.urlNotFound }
+
+		let (data, response) = try await URLSession.shared.data(from: articleURL)
+
+		guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw ApiError.badHttpResponse }
+
+		do {
+			return try JSONDecoder().decode(AstronomyArticleModel.self, from: data)
+		} catch {
+			throw error
+		}
+	}
+}
